952ae7eec9612714e59164aefb6f2823e96c791f
Vooray <inbox.mikhail@gmail.com>
2020-06-20 16:27:28 +0300

    renames

--- cgi-bin/sub.py
+++ cgi-bin/sub.py
@@ -2,11 +2,11 @@ import cgi
 import html
 import ipaddress
 
-sub_pc = ipaddress.ip_network('10.30.0.0/15')
-sub_voip = ipaddress.ip_network('10.32.0.0/15')
+supnet_pc = ipaddress.ip_network('10.30.0.0/15')
+supnet_voip = ipaddress.ip_network('10.32.0.0/15')
 
-sub_pc_subnets = list(sub_pc.subnets(new_prefix=25))
-sub_voip_subnets = list(sub_voip.subnets(new_prefix=25))
+subnets_pc = list(supnet_pc.subnets(new_prefix=25))
+subnets_voip = list(supnet_voip.subnets(new_prefix=25))
 
 print("Content-type: text/html\n")
 print("""<!DOCTYPE HTML>
@@ -17,26 +17,25 @@ print("""<!DOCTYPE HTML>
             <title>BO subnet calc</title>
         </head>
         <body>
-            <h1>/// BO subnet calc</h1>
-            Office num:
+            <h1>-> Branch Office subnet calculator <-</h1>
+            Office number:
             <form>
                 <form action="/cgi-bin/bo_sub.py">
-                <input type="text" name="BO_NUM">
+                <input type="text" name="OFFICE_NUM">
                 <input type="submit">
             </form>
         """)
 
 form = cgi.FieldStorage()
-bo_num = form.getfirst("BO_NUM")
-bo_num = int(html.escape(bo_num))
+office_num = form.getfirst("OFFICE_NUM")
+office_num = int(html.escape(office_num))
 
 print("<hr>")
+print("<h2>For office number: " + str(office_num) + "</h2>")
 
-print("<h2>For office number: " + str(bo_num) + "</h2>")
-
-if int(bo_num) >= len(sub_pc_subnets):
+if int(office_num) >= len(subnets_pc):
     print('<h2 style="color:Tomato;">Out of range!</h2>')
-    print('<h2 style="color:Tomato;">Maximum office num is ' + str(len(sub_pc_subnets)-1) + '</h2>')
+    print('<h2 style="color:Tomato;">Maximum office number is ' + str(len(subnets_pc)-1) + '</h2>')
 else:
     print("""<table style="width:40%">
                 <tr>
@@ -57,10 +56,10 @@ else:
                     </td>
                     <td>
                     """)
-    print(sub_pc_subnets[bo_num])
+    print(subnets_pc[office_num])
     print("""</td>
                     <td>""")
-    print(sub_voip_subnets[bo_num])
+    print(subnets_voip[office_num])
     print("""
                     </td>
                 </tr>
@@ -71,11 +70,11 @@ else:
                         </b>
                     </td>
                     <td>""")
-    print(list(sub_pc_subnets[bo_num].hosts())[0])
+    print(list(subnets_pc[office_num].hosts())[0])
     print("""
                     </td>
                     <td>""")
-    print(list(sub_voip_subnets[bo_num].hosts())[0])
+    print(list(subnets_voip[office_num].hosts())[0])
     print("""
                     </td>
                 </tr>
@@ -86,14 +85,15 @@ else:
                         </b>
                     </td>
                     <td>""")
-    print(sub_pc_subnets[bo_num].netmask)
+    print(subnets_pc[office_num].netmask)
     print("""
                     </td>
                     <td>""")
-    print(sub_voip_subnets[bo_num].netmask)
+    print(subnets_voip[office_num].netmask)
     print("""
                     </td>
                 </tr>
             </table>
         </body>
     """)
+    print('</body>')
